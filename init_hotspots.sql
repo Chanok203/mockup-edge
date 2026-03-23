4475f677b3c1a4eb3b11f4d7d22f726acae9ceed
Chanok Pathompatai <cpathompatai@gmail.com>
2026-03-24 03:17:13 +0700

    add init_hotspots

---
+++ init_hotspots.sql
@@ -0,0 +1,4 @@
+INSERT INTO "hotspots" (type, latitude, longitude, geometry, confidence, created_at) VALUES 
+('PRED_30', 0, 0, '{"type":"Point","coordinates":[0,0]}', 30, NOW()),
+('PRED_45', 0, 0, '{"type":"Point","coordinates":[0,0]}', 45, NOW()),
+('PRED_60', 0, 0, '{"type":"Point","coordinates":[0,0]}', 60, NOW());
