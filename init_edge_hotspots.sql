88e5173660d092ef79bc4d433d6984837f6d18bb
Chanok Pathompatai <cpathompatai@gmail.com>
2026-03-24 03:11:50 +0700

    add init_hotspots

---
+++ init_edge_hotspots.sql
@@ -0,0 +1,3 @@
+INSERT INTO "edge"."hotspots_p30" (geom) VALUES (ST_SetSRID(ST_MakePoint(0, 0), 4326));
+INSERT INTO "edge"."hotspots_p45" (geom) VALUES (ST_SetSRID(ST_MakePoint(0, 0), 4326));
+INSERT INTO "edge"."hotspots_p60" (geom) VALUES (ST_SetSRID(ST_MakePoint(0, 0), 4326));
